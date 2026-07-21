f259bb37d255a2c4680476bc0c0618b57ffce72c
Jakub-knap <109682058+Jakub-knap@users.noreply.github.com>
2026-07-21 12:56:18 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* CAMP SYNC — service worker
    Pri každom nasadení novej verzie HTML bumpni číslo CACHE! */
-const CACHE = 'campsync-v37';
+const CACHE = 'campsync-v38';
 
 const SHELL = [
   './app.html',
